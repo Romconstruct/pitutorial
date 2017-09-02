147ba89e92cc1be996166959f8082382f652afd9
Romconstruct <git@romconstruct.org>
2017-09-02 13:17:25 +0200

    changed program to blinking with for loop

--- led_1.py
+++ led_1.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
 import time
-#PIN-Nummern verwenden
+
+#use GPIO PIN-Numbers
 GPIO.setmode(GPIO.BOARD)
-# PIN 7 = GPIO 7
+
+# PIN 7 equals GPIO 7
 GPIO.setup(7, GPIO.OUT)
 
-# Pin 7 einschalten
+# Set PIN 7
 GPIO.output(7, GPIO.HIGH)
-# 5 Sekunden sleep
-time.sleep(5)
-GPIO.output(7, GPIO.LOW)
 
-# wieder freigeben
+# Blink for 5 times for 3 seconds
+for x in range (0, 5):
+   time.sleep(3) 
+   GPIO.output(7, GPIO.LOW)
+   time.sleep(3)
+   GPIO.output(7, GPIO.HIGH)
+
+# reset PIN
 GPIO.cleanup()
 
 
