f60f78851c283d5056eed539d5fe63dcfa359e50
Romconstruct <git@romconstruct.org>
2017-09-10 09:40:27 +0200

    comments and some code cleanup

--- led_1.py
+++ led_1.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
-# small LED blinking script with GPIO Board
-# script assumes fix GPIO port 7 is usec
+#-----------------------------------------------------
+# File name     : led_1.py
+# Despription   : Blinking LED with user input
+# Author        : Romconstruct
+# E-mail        : mail@romconstruct.org
+# Website       : www.romconstruct.org
+# Date          : 2017/09/10
+#----------------------------------------------------
 
+# Imports
 import RPi.GPIO as io
 import sys
 import time
+
+# define LED Port number
+ledPin = 7 # PIn 7 equals GPIO 7
 
 try:
     # user input how often and how long LED should blink
@@ -14,28 +24,26 @@ try:
 
     #use GPIO PIN-Numbers
     io.setmode(io.BOARD)
+    io.setup(ledPin, io.OUT)
 
-    # PIN 7 equals GPIO 7
-    io.setup(7, io.OUT)
-
-    # Blink for 5 times for 3 seconds
+    # Blink for counter times for lightTime seconds
     for x in range (0, counter):
-        io.output(7, 1) 
+        print ("...led on for " +str(x+1) +" time(-s)")
+        io.output(ledPin, io.HIGH) # led on
         time.sleep(lightTime) 
-        io.output(7, 0)
+        print ("...led off")
+        io.output(ledPin, io.LOW) # led off
         time.sleep(lightTime)
 
-except KeyboardInterrupt:
-    # CTRL-C exit
+except KeyboardInterrupt: # manual exit on Ctrl-C
     print ("Manual user exit occured!")
 
-except:
-    # exception output
+except: # exception handling
     print ("Some error or exception occured!")
     sys.exc_info()[0]
     raise
 finally:
-    # reset PIN
-    io.cleanup()
+    io.output(ledPin, io.LOW) # led off
+    io.cleanup() # release ressource
 
 
